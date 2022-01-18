0ce5641e740ac84f70ee6d0459bf97624407c5ee
EricMainhard <mainhard.eric@gmail.com>
2022-01-17 23:09:32 -0300

    adding icons

--- src/Components/TodoItem.jsx
+++ src/Components/TodoItem.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
+import {HiOutlineReply,HiOutlineCheck, HiX} from "react-icons/hi";
 
 function TodoItem({task,completed,completeTodos,dataset,deleteTodos}) {
     return (
         <div className='todoItem'>
             <div className="todo-controls">
-                <i className='symbol complete-symbol' onClick={()=>{completeTodos(dataset)}}>
-                    {!completed ? '✅' : '🔄'}</i>
-                <i className='symbol delete-symbol' onClick={()=>{deleteTodos(dataset)}}>❎</i>
+                <i className='icon complete-icon' onClick={()=>{completeTodos(dataset)}}>
+                    {!completed ? <HiOutlineCheck/> : <HiOutlineReply/> } 
+                </i>
+                <i className='icon delete-icon fas fa' onClick={()=>{deleteTodos(dataset)}}>
+                    <HiX/>
+                </i>
             </div>
             <p className={completed === true ? 'complete' : 'pending'}>{task}</p>
         </div>
