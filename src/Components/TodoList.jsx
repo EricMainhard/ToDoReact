fd17d53891e5312cccf0cf9ae8ec583561f5f7f2
Eric Mainhard <eric.mainhard@vnstudios.com>
2022-01-21 20:51:24 -0300

    Remove second page state

--- src/Components/TodoList.jsx
+++ src/Components/TodoList.jsx
@@ -1,14 +1,6 @@
-import React, {useState,useEffect} from 'react'
+import React from 'react'
 
 function TodoList(props) {
-    
-const [secondPage,setSecondPage] = useState(false)
-
-useEffect(()=>{
-    if(props.children[3].length > 3){
-        console.log('slide') 
-    }
-},[props.children[3]])
     
     return (
         <section className='todoList'>  
