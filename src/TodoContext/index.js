422513ecea29f658f53f2ff09ae57352f22b044f
Eric Mainhard <eric.mainhard@vnstudios.com>
2022-01-21 20:47:54 -0300

    Add React import to TodoContext

--- src/TodoContext/index.js
+++ src/TodoContext/index.js
@@ -1,4 +1,4 @@
-import { createContext,useState } from "react";
+import React,{createContext,useState } from "react";
 import {useLocalStorage} from "./useLocalStorage";
 
 const TodoContext = createContext();
