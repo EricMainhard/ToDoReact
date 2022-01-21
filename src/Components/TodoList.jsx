af8004c7e91399f28526df1db1fe112093a860ba
Eric Mainhard <eric.mainhard@vnstudios.com>
2022-01-21 20:44:07 -0300

    Build folder

--- src/Components/TodoList.jsx
+++ src/Components/TodoList.jsx
@@ -1,7 +1,15 @@
-import React from 'react'
-import TodoItem from './TodoItem';
+import React, {useState,useEffect} from 'react'
 
 function TodoList(props) {
+    
+const [secondPage,setSecondPage] = useState(false)
+
+useEffect(()=>{
+    if(props.children[3].length > 3){
+        console.log('slide') 
+    }
+},[props.children[3]])
+    
     return (
         <section className='todoList'>  
             {props.children}
